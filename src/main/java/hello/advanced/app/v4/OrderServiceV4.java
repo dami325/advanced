7b16f19cdba7e9286bf4525b702ae96c7cd314be
Judalm Park (dami) <wnekfa1004@naver.com>
2023-05-11 20:55:42 +0900

    템플릿 콜백 패턴 - 적용

--- src/main/java/hello/advanced/app/v4/OrderServiceV4.java
+++ src/main/java/hello/advanced/app/v4/OrderServiceV4.java
@@ -24,7 +24,7 @@ public class OrderServiceV4 {
                 return null;
             }
         };
-        template.execute("OrderServiceV4.orderItem()");
+        template.execute("OrderServiceV5.orderItem()");
 
     }
 }
